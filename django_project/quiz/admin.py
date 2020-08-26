02327189708abf4a78ec93027d2b09d46f082e9f
AlphaTechnic <58129950+AlphaTechnic@users.noreply.github.com>
2020-08-26 11:07:32 +0900

    voicequizlet 1차 완성

---
+++ django_project/quiz/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Card)
+admin.site.register(Quiz)
+admin.site.register(Search_time)
+admin.site.register(Download_time)
+admin.site.register(Make_time)
+admin.site.register(Login)
+
+
---
+++ django_project/quiz/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+class Card(models.Model):
+    user_name = models.CharField(max_length=100)
+    cards = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+    title = models.CharField(max_length=100, null=True)
+    description = models.TextField(null=True)
+    hashtag = models.TextField(max_length=100, null=True)
+    likes = models.IntegerField(default=0, null=True)
+    downloads = models.IntegerField(default=0, null=True)
+    number_of_quiz = models.IntegerField(default=0, null=True)
+
+    def __str__(self):
+        return self.title
+
+class Quiz(models.Model):
+    card = models.ForeignKey(Card, on_delete=models.CASCADE)
+    question = models.TextField(max_length=200, null=True)
+    answer = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.answer
+
+class Login(models.Model):
+    user_name = models.CharField(max_length=100, null=True)
+    time = models.DateTimeField(auto_now_add=True, null=True)
+
+class Search_time(models.Model):
+    keyword =models.TextField(max_length=200, default=None, null=True)
+    time = models.DateTimeField(auto_now_add=True, null=True)
+
+class Download_time(models.Model):
+    card_title = models.TextField(max_length=200, default=None, null=True)
+    time = models.DateTimeField(auto_now_add=True, null=True)
+    card = models.ForeignKey(Card, on_delete=models.CASCADE)
+
+class Make_time(models.Model):
+    card_title = models.TextField(max_length=200, default=None, null=True)
+    time = models.DateTimeField(auto_now_add=True, null=True)
